b00676ccb13d33aaf2e280e1d083f5f4cc2a42f9
XElderX <dalius.badley@gmail.com>
2022-08-03 13:26:14 +0300

    fixed menu model's filable method issues

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
-    public $fillable = ['title', 'restourant'];
+    public $fillable = ['menu_title', 'restourant_id'];
 
     public function dishes(){
         return $this->hasMany(Dish::class);
